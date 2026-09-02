d7ab1202cc3ee758e18340ab96a18908708d9316
Maruf Hussen <marufhussen50414@gmail.com>
2026-09-02 22:47:31 +0200

    Update page.tsx

--- app/disputes/page.tsx
+++ app/disputes/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 
+import { AppShell } from '@/components/app-shell';
 import { DisputesReportsView } from '@/components/sections/disputes-reports';
 
 export default function DisputesPage() {
-  return <DisputesReportsView defaultTab="disputes" />;
+  return (
+    <AppShell title="Disputes & Reports">
+      <DisputesReportsView defaultTab="disputes" />
+    </AppShell>
+  );
 }
