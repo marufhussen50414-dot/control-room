66229578fa467c0dda1140666c10bc7011aca668
Maruf Hussen <marufhussen50414@gmail.com>
2026-09-02 21:34:29 +0200

    Create page.tsx

---
+++ app/reports/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { DisputesReportsView } from '@/components/sections/disputes-reports';
+
+export default function ReportsPage() {
+  return <DisputesReportsView defaultTab="reports" />;
+}
